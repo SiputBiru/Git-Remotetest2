d26e615f19ac4548dabe5119e1e7c42bc909ffe1
SiputBiru <133071313+SiputBiru@users.noreply.github.com>
2024-01-31 11:15:16 +0700

    meanambahkan uwu.php

--- uwu.php
+++ uwu.php
@@ -3,16 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <<link rel="stylesheet" type="text/css" href="style.css">
+    <title>Document uwu</title>
 </head>
 <body>
     <<h1>
-        Tomi wibu
+        asdfljlkajsdf
     </h1>
 </body>
 </html>
-
-<?php
-
-?>
